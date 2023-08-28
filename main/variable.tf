33cd7a37e92d7819e29cc6ca58d90a2ecd94faba
Sandip Gangapadhyay <gangapad@amazon.com>
2023-08-28 12:21:43 -0400

    updated security context

--- main/variable.tf
+++ main/variable.tf
@@ -1,14 +1,14 @@
 variable "region" {
   description = "set the region for deployment"
-  default     = "us-east-2"
+  default     = "us-west-1"
 }
 
 variable "eks_cluster_name" {
   description = "Name of the EKS Cluster"
-  default     = "demo2"
+  default     = "mycrdb1"
 }
 
 variable "number_of_nodes" {
   description = "Number of nodes to be deployed"
-  default     = 4
+  default     = 3
 }
